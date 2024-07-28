cf639b7e8dd5d3c9853ef766db2ce55b70bb7cbc
Muhammad <92302413+MuhamadBoboev@users.noreply.github.com>
2024-07-28 22:53:43 +0500

    change url

--- src/shared/api/axios-instance.ts
+++ src/shared/api/axios-instance.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 
 export const api = axios.create({
-  // baseURL: import.meta.env.VITE_APP_API_URL,
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_APP_API_URL
 })
 
